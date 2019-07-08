378770d0f602f242e5d6ef6b7eb2f7ab1ad9a5fb
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-07-08 13:42:48 +0200

    Feature: express app

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // implement your API here
+const express = require('express');
+const server = express();
+// const Hub = require()
+
+server.use(express.json());
+
+server.get('', (req, res) => {
+
+});
+
+// server.get('', (req, res) => {
+
+// });
+
+// server.delete('', (req, res) => {
+
+// });
+
+// server.post('', (req, res) => {
+
+// });
+
+// server.put('', (req, res) => {
+
+// });
+
+server.listen(3000, () => {
+  console.log('listen on port 3000');
+})
