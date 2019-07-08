87651cb8b3990c359554b39114ef9ef8b6d16931
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-07-08 17:02:34 +0200

    Feature: added axios methods and content

--- frontandback/src/App.js
+++ frontandback/src/App.js
@@ -1,26 +1,79 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      data: '',
+     }
+  }
+
+  componentDidMount = () => {
+    axios.get('http://localhost:3000/api/users')
+    .then(res => {
+      this.setState({
+        data: res.data.result
+      })
+    })
+    .catch(err => console.log(err))
+  };
+
+  delete = (id) => {
+    axios.delete(`http://localhost:3000/api/users/${id}`)
+    .then(res => {
+      debugger
+      this.setState({
+        data: res.data.result,
+      })
+    })
+    .catch(err => console.log(err))
+  };
+
+  update = (id) => {
+    axios.put(`http://localhost:3000/api/users/${id}`)
+    .then(res => {
+      this.setState({
+        data: res.data.result,
+      })
+    })
+    .catch(err => console.log(err))
+  };
+
+  post = () => {
+// const new = {
+// }
+
+    axios.post('http://localhost:3000/api/users')
+    .then(res => {
+      this.setState({
+        data: res.data.result,
+      })
+    })
+    .catch(err => console.log(err))
+  };
+  
+  render() { 
+    if(this.state.data){
+      return <div> 
+      {this.state.data.map(item => {
+        return <div>
+          <div>{item.name}</div>
+        <div>{item.bio}</div>
+        <button onClick={() => this.delete(item.id)}>X</button>
+        <button onClick={() => this.update(item.id)}>update</button>
+        </div>
+      })}
+<button onClick={this.post}>add</button>
+</div>
+    }
+    return ( 
+      <div className="App">
+     
     </div>
-  );
+     );
+  }
 }
-
+ 
 export default App;
